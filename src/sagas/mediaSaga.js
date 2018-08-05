307f568d694cf8b0f63a4ae5761fca5a6f4880ac
celcoder <celcoder@gmail.com>
2018-08-04 20:06:03 -0400

    true parallel call

--- src/sagas/mediaSaga.js
+++ src/sagas/mediaSaga.js
@@ -1,14 +1,17 @@
-import { put, call } from 'redux-saga/effects';
-import { flickrImages, shutterStockVideos } from '../Api/api';
-import * as types from '../constants/actionTypes';
+import { put, call } from "redux-saga/effects";
+import { flickrImages, shutterStockVideos } from "../Api/api";
+import * as types from "../constants/actionTypes";
 
 // Responsible for searching media library, making calls to the API
 // and instructing the redux-saga middle ware on the next line of action,
 // for success or failure operation.
 export function* searchMediaSaga({ payload }) {
   try {
-    const videos = yield call(shutterStockVideos, payload);
-    const images = yield call(flickrImages, payload);
+    const [videos, images] = yield [
+      call(shutterStockVideos, payload),
+      call(flickrImages, payload)
+    ];
+
     yield [
       put({ type: types.SHUTTER_VIDEOS_SUCCESS, videos }),
       put({ type: types.SELECTED_VIDEO, video: videos[0] }),
@@ -16,6 +19,6 @@ export function* searchMediaSaga({ payload }) {
       put({ type: types.SELECTED_IMAGE, image: images[0] })
     ];
   } catch (error) {
-    yield put({ type: 'SEARCH_MEDIA_ERROR', error });
+    yield put({ type: "SEARCH_MEDIA_ERROR", error });
   }
 }
